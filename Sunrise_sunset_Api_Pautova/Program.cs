02d628673aea86496baf98c50dae772b8260b91b
Opilane TTHK <opilane@tthk.ee>
2020-10-13 09:56:25 +0300

    First Lesson

---
+++ Sunrise_sunset_Api_Pautova/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Net;
+
+namespace Sunrise_sunset_Api_Pautova
+{
+    class Program
+    {
+        private static string url = "https://api.sunrise-sunset.org/json?lat=59.4370&lng=24.7536&date=2020-10-13";
+        static void Main(string[] args)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            var WebResponse = request.GetResponse();
+            var WebStream = WebResponse.GetResponseStream();
+            using (var responsereader = new StreamReader(WebStream))
+            {
+                var response = responsereader.ReadToEnd();
+                SunMode sunMode = JsonConvert.DeserializeObject<SunMode>(response);
+                Console.WriteLine($"Sunrise: {sunMode.Results.Sunrise}.");
+                Console.WriteLine($"Sunset: {sunMode.Results.Sunrise}.");
+                Console.WriteLine($"Status: {sunMode.Results.Sunrise}.");
+
+            }
+        }
+    }
+}
---
+++ Sunrise_sunset_Api_Pautova/SunMode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sunrise_sunset_Api_Pautova
+{
+    public class SunMode
+    {
+        public Results Results { get; set; }
+        public string Status { get; set; }
+    }
+    public class Results
+    {
+        public string Sunrise { get; set; }
+        public string Sunset { get; set; }
+    }
+}
